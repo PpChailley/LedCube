419e207edf3bd95857f31b605a676515d9badaa7
PPC <ppchailley@gmx.com>
2017-11-04 17:28:14 +0100

    CODE Macros to command PWM

--- Pinout.h
+++ Pinout.h
@@ -3,6 +3,38 @@
 
   #include <Arduino.h>
 
+
+  /**
+   *   Bit macros 
+   */
+  #define   NOT(l)   (l==HIGH?LOW:HIGH)
+  
+  #define   IF_BITSET(data, bitIndex, level)    \
+              (  (d&(1<<i)) ? level : NOT(level) ) 
+
+  #define   DRIVE(pin, level, duration)         \
+                digitalWrite(pin, level);       \
+                delay(duration);                \
+                digitalWrite(pin, NOT(level));  
+
+  #define   FADELOW(pin, duration)                                    \
+                for (int fadeIndex=255; fadeIndex >=0; fadeIndex--)   \
+                {                                                     \
+                  analogWrite(pin,fadeIndex);                         \
+                  delay(duration/256);                                \
+                }                    
+
+  #define   FADEHIGH(pin, duration)                                    \
+                for (int fadeIndex=0; fadeIndex < 256; fadeIndex++)   \
+                {                                                      \
+                  analogWrite(pin,fadeIndex);                          \
+                  delay(duration/256);                                 \
+                }                    
+              
+
+  /**
+   *   Pinout
+   */
   #define DATA(x)   (12-x)
   #define OUTENA_     14
   #define CLK         2
