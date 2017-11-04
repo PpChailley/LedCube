fa1f5a8c441202ef2d3ddb52d41b51fad4a6be0b
PPC <ppchailley@gmx.com>
2017-11-04 15:07:10 +0100

    REFACTOR Cleanup some pinout test code

--- Pinout.h
+++ Pinout.h
@@ -3,37 +3,7 @@
 
   #include <Arduino.h>
 
-
-
-
-
-
-
-  #define PIN_D2    2   // OK
-  #define PIN_D3    3   // OK
-  #define PIN_D4    4   // OK
-  #define PIN_D5    5   // OK
-  #define PIN_D6    6     // OK
-  #define PIN_D7    7     // OK
-  #define PIN_D8    8     // OK
-  #define PIN_D9    9
-  #define PIN_D10   10    // OK
-  #define PIN_D11   11
-  #define PIN_D12   12    // OK
-
-
-
-  #define DATA(x)       \
-        (x==0? PIN_D12: \
-        (x==1? PIN_D11: \
-        (x==2? PIN_D10: \
-        (x==3? PIN_D9: \
-        (x==4? PIN_D8: \
-        (x==5? PIN_D7: \
-        (x==6? PIN_D6: \
-        (x==7? PIN_D5: -1 ))))))))
-  
-  
+  #define DATA(x)   (12-x)
   #define OUTENA_     14
   #define CLK         2
   #define PLANE_SEL   4
